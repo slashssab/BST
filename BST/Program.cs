663502c2b0c66da1292920a837edddd4ed486284
lordsitow <chusarz@gmail.com>
2018-03-26 19:24:25 +0200

    test komentarza

--- BST/Program.cs
+++ BST/Program.cs
@@ -16,6 +16,7 @@ namespace BST
                 if (heap.Max() > prev) throw new Exception("Error in Heap");
                 prev = heap.Max();
                 heap.DeleteMax();
+                //////////////////////////////////sdjkfhli
             }
         }
 
